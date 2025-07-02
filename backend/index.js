79d13b3dedc51b426a70b344d073478b697c87ac
Reena Vollala <reenav@meta.com>
2025-07-02 16:32:20 -0700

    removed quotes

--- backend/index.js
+++ backend/index.js
@@ -23,7 +23,7 @@ app.use(cors({
 app.use(express.json())
 
 app.use(session({
-  secret: 'process.env.SESSION_SECRET', 
+  secret: process.env.SESSION_SECRET, 
   resave: false,
   saveUninitialized: false,
   cookie: { secure: false, httpOnly: true, maxAge: 1000 * 60 * 60 } // 1-hour session
