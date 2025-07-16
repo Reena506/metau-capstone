5709f037661c357eec5f2fed3c6c2ab8842f324d
Reena Vollala <reenav@meta.com>
2025-07-16 14:40:38 -0700

    added all routes and components to app

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,16 +5,25 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import { Navigate } from "react-router-dom";
 import SignupForm from './components/SignupForm';
 import LoginForm from './components/LoginForm';
+import MyTrips from './components/MyTrips';
 import { useUser } from './contexts/UserContext';
 import Navbar from './components/Navbar';
+import EditTrip from './components/EditTrip';
+import TripDetail from './components/TripDetail';
+import Notes from './components/Notes';
+import Suggestions from './components/Suggestions';
 import './App.css'
+import TripNavBar from './components/TripNavBar';
+import Itinerary from './components/Itinerary';
+import ExpenseTracker from './components/ExpenseTracker';
+const APP_URL=import.meta.env.VITE_APP_URL
 
 const App = () => {
   const { user, setUser } = useUser();
 
 
   useEffect(() => {
-  fetch("http://localhost:3000/me", { credentials: "include" })
+  fetch(`${APP_URL}/me`, { credentials: "include" })
     .then((res) => {
       if (!res.ok) throw new Error("Not logged in");
       return res.json();
@@ -34,13 +43,22 @@ const App = () => {
     <Router>
       <main>
         <header>
-          <h1 className='title'>MyTravel</h1>
           <Navbar/>
         </header>
         <Routes>
            <Route path="/" element={<Navigate to="/login" />} />
           <Route path="/signup" element={<SignupForm />} />
           <Route path="/login" element={<LoginForm />} />
+          <Route path="/my-trips" element={<MyTrips/>}/>
+          <Route path="/trips/:tripId/edit" element={<EditTrip/>}/> 
+           <Route path="/trips/:tripId" element={<TripNavBar/>}> 
+            <Route index element={<TripDetail/>}/>
+             <Route path="notes" element={<Notes/>}/> 
+              <Route path="suggestions" element={<Suggestions/>}/> 
+              <Route path="itinerary" element={<Itinerary/>}/> 
+              <Route path="expenses" element={<ExpenseTracker/>}/> 
+              
+           </Route>
         </Routes>
       </main>
     </Router>
