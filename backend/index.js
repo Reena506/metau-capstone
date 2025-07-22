2c6ecc55e9571579b4d1691d2d11596f6eb56f65
Reena Vollala <reenav@meta.com>
2025-07-22 15:15:59 -0700

    backend for custom budject allocation

--- backend/index.js
+++ backend/index.js
@@ -28,6 +28,35 @@ app.use(session({
   saveUninitialized: false,
   cookie: { secure: false, httpOnly: true, maxAge: 1000 * 60 * 60 } // 1-hour session
 }))
+app.get("/trips/allocation-presets", (req, res) => {
+  const presets = {
+    balanced: {
+      Food: 30,
+      Transport: 10,
+      Lodging: 25,
+      Activities: 20,
+      Shopping: 10,
+      Other: 5,
+    },
+    luxury: {
+      Food: 25,
+      Transport: 20,
+      Lodging: 35,
+      Activities: 15,
+      Shopping: 3,
+      Other: 2,
+    },
+    budget: {
+      Food: 40,
+      Transport: 15,
+      Lodging: 30,
+      Activities: 10,
+      Shopping: 3,
+      Other: 2,
+    }
+  };
+  res.json({presets});
+});
 app.use(authRoutes)
 app.use('/trips', triproutes)
 app.use('/trips', eventroutes)
